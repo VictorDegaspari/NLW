aef3f5de1f7b71c65dc4798c95b652e316b8ac71
Victor Degaspari <vdegaspari.vm@gmail.com>
2020-08-06 14:57:01 -0300

    Header Updated

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -5,7 +5,12 @@ function TeacherForm() {
     
     return (
         <div id="page-teacher-list" className="container">
-        <PageHeader title = "Que incrível que você quer dar aulas!"/>
+        <PageHeader 
+        title = "Que incrível que você quer dar aulas!"
+        description = "O primeiro passo é preencher esse formulário de descrição."
+        
+        />
+        
         </div>
     )
      
