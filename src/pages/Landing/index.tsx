0ef2c8343217b487c71ca4b3f24efbdb4e4d4ae9
Victor Degaspari <vdegaspari.vm@gmail.com>
2020-08-07 17:01:15 -0300

    Conectando api com o front

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoimg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -7,8 +7,21 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import { Link } from 'react-router-dom'
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing() {
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() =>{
+        api.get('/connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
+
+
     return (
         <div id="page-landing">
             <div 
@@ -39,7 +52,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 inscrições já realizadas
+                    Total de {totalConnections} inscrições já realizadas
 
                     <img src={purpleHeartIcon} 
                     alt="Coração roxo"
